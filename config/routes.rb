e28441de2a6611d502e0280cecfe606df0053c03
jared w <jaredtimothywillis@gmail.com>
2018-01-16 08:58:44 -0800

    redo intro crud checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
 
   resources :posts
 
-  get 'about' => 'welcome/#about'
-
-  get 'welcome/contact'
-
-  get 'welcome/faq'
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
 
