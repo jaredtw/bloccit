b4735528eedb16a1229bc8fb3616604d099b98f7
jared w <jaredtimothywillis@gmail.com>
2018-01-16 09:33:59 -0800

    merge code error

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   resources :posts
 
   get 'about' => 'welcome#about'
-<<<<<<< HEAD
 
   get 'welcome/contact'
 
   get 'welcome/faq'
-=======
->>>>>>> _CRUD
 
   root 'welcome#index'
 
