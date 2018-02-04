6071a1d03dc7c7a02994f064790af9814d126116
jared w <jaredtimothywillis@gmail.com>
2018-02-03 23:54:46 -0800

    ckp 27 assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
+#  before_action :authorize_user, except: [:show, :new, :create]
   def show
     @post = Post.find(params[:id])
   end
@@ -25,44 +25,63 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You do not have permission to do that"
+      redirect_to [post.topic, post]
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   def update
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-    @post.assign_attributes(post_params)
-
-    if @post.save
-      flash[:notice] = "Post was updated."
-      redirect_to [@post.topic, @post]
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You do not have permission to do that"
+      redirect_to [post.topic, post]
     else
-      flash.now[:alert] = "There was an error saving the post. Please try again."
-      render :edit
+      @topic = Topic.find(params[:topic_id])
+      @post = Post.find(params[:id])
+      @post.assign_attributes(post_params)
+
+      if @post.save
+        flash[:notice] = "Post was updated."
+        redirect_to [@post.topic, @post]
+      else
+        flash.now[:alert] = "There was an error saving the post. Please try again."
+        render :edit
+      end
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
-
-    if @post.destroy
-      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to @post.topic
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin?
+      flash[:alert] = "You do not have permission to do that"
+      redirect_to [post.topic, post]
     else
-      flash.now[:alert] = "There was an error deleting the post."
-      render :show
+      @post = Post.find(params[:id])
+
+      if @post.destroy
+        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+        redirect_to @post.topic
+      else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
+      end
     end
   end
+
   private
   def post_params
     params.require(:post).permit(:title, :body)
   end
 
-  def authorize_user
-    post = Post.find(params[:id])
-    unless current_user == post.user || current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to [post.topic, post]
-    end
-  end
+#  def authorize_user
+#    post = Post.find(params[:id])
+#    unless current_user == post.user || current_user.admin?
+#      flash[:alert] = "You must be an admin to do that."
+#      redirect_to [post.topic, post]
+#    end
+#  end
 end
