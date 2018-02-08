0f09b81632ceaa94b49ca2527a81d3b54ae9205a
jared w <jaredtimothywillis@gmail.com>
2018-02-08 08:10:11 -0800

    checkpoint 30 code missing

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,26 @@
+class FavoriteMailer < ApplicationMailer
+  default from: "jaredtimothywillis@gmail.com"
+
+  def new_comment(user, post, comment)
+
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, cc: "christine.le@bloc.io", subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following, #{post.title}")
+  end
+end
