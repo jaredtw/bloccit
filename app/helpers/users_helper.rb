86dd3edb1a785ea3d6d13611956a7043a964ff08
jared w <jaredtimothywillis@gmail.com>
2018-02-11 20:32:55 -0800

    assignment 31 wip

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,25 @@
 module UsersHelper
-  def user_has_posts?
-    @user.posts.any?
+  def user_has_posts?(user)
+    if user.posts.any?
+      render(user.posts)
+    else
+      content_tag(:p, "#{user.name} has not submitted any posts yet.")
+    end
   end
 
-  def user_has_comments?
-    @user.comments.any?
+  def user_has_comments?(user)
+    if user.comments.any?
+      render(user.comments)
+    else
+      content_tag(:p, "#{user.name} has not commented on any posts yet.")
+    end
   end
 
-  def user_has_favorites?
-    @user.favorites.any?
+  def user_has_favorites?(user)
+    if user.favorites.any?
+      render(user.favorites)
+    else
+      content_tag(:p, "#{user.name} has not favorited anything yet.")
+    end
   end
 end
